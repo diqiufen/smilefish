ef5915c319214942328e1396cfa287f772c58947
diqiufen <1191640211@qq.com>
2020-04-22 01:27:02 +0800

    随便写点

---
+++ VoladorMessage/app/src/main/java/com/smle/fish/multimedia/MediaController.java
@@ -0,0 +1,15 @@
+package com.smle.fish.multimedia;
+
+import com.google.android.exoplayer2.SimpleExoPlayer;
+
+/**
+ * 媒体控制器
+ *    https://codelabs.developers.google.com/codelabs/exoplayer-intro/#1
+ */
+public class MediaController {
+
+    SimpleExoPlayer simpleExoPlayer;
+    MediaController(){
+
+    }
+}
